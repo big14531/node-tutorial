a5a8a25de7af705c0504b9db0eaf8a2b19d05477
Yuthapichai Channit <yuthapichai14531@gmail.com>
2017-08-29 14:05:27 +0700

    Remove some config

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
 
     'facebookAuth' : {
-        fb_client_id : '1258184930930443',
-        fb_secert_id : '248a0cbd709f8b2f26bc0e5002969db8',
+        fb_client_id : '**********',
+        fb_secert_id : '**********',
     },
 
     'twitterAuth' : {
-        'consumer_key'          : 'cq3j0YbjIUOw2e02P1r9qR1gc',
-        'consumer_secret'       : 'SVuxRIIiZqHidFkqyMer0vUJ22UQ1iw8tu8J3iVfunQC5FPqcM',
-        'access_token_key'      : '2671886117-R2hV6oDxiVZynNSBSVt1Ja64EXbO8bC5PO6O2b6',
-        'access_token_secret'   : 'R6sBu2n8sWUMXs2Tl5n8BJ48oHleZe9cza68LY7CphHuo'
+        'consumer_key'          : '**********',
+        'consumer_secret'       : '**********',
+        'access_token_key'      : '**********',
+        'access_token_secret'   : '**********'
     }
 };
